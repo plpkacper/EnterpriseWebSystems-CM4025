4ff001a38f4694be21c2dbd204a254f900f9180c
plpkacper <43370662+plpkacper@users.noreply.github.com>
2023-02-02 14:14:56 +0000

    Commit Install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+cd Desktop/
+mkdir CM4025EWS
+cd CM4025EWS/
+sudo apt-get install git-all
+sudo apt-get install node
+git clone https://github.com/plpkacper/EnterpriseWebSystems-CM4025.git
+cd EnterpriseWebSystems-CM4025/
+cd Lab1/
+ls
+sudo apt-get install npm
+npm install express
+npm install ejs
+sudo ufw allow 8080/tcp
+node index.js
