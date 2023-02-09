d7d50b5754553d1aa95dfadc543716048a9e7366
plpkacper <43370662+plpkacper@users.noreply.github.com>
2023-02-09 14:29:53 +0000

    Update to add mongodb

--- Lab1/public/scripts/myWebPage.js
+++ Lab1/public/scripts/myWebPage.js
@@ -7,14 +7,20 @@ $(document).ready(function(){
     $("#calcButton").click(function() {
         var s = $("#salary").val();
         var d = $("#days").val();
+        var n = $("#quoteName").val();
         console.log("Calculating price");
         console.log(s);
         console.log(d);
+        console.log(n);
         let finalPrice = 0;
         dailyRate = s/365;
         finalPrice = dailyRate * d;
         finalPriceAdjusted = Math.round(finalPrice / 50)*50;
         $("#finalPrice").html(finalPriceAdjusted);
+        $.post('/saveQuote', { n: $("#quoteName").val(), s: $("#salary").val(), d: $("#days").val()});
     })
+
 });
-    
+
+
+
